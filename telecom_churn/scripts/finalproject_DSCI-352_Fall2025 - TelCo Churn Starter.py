dd515e04fc595fa46ce736fb81522de4dbb94a6b
isaaclw1 <ilwang@usc.edu>
2025-12-08 16:45:44 -0800

    Here

--- telecom_churn/scripts/finalproject_DSCI-352_Fall2025 - TelCo Churn Starter.py
+++ telecom_churn/scripts/finalproject_DSCI-352_Fall2025 - TelCo Churn Starter.py
@@ -296,8 +296,7 @@ def train_sklearn_models(preprocessor, X, y):
             RandomForestClassifier(
                 n_estimators=100,
                 max_depth=10,
-                random_state=42,
-                n_jobs=-1
+                random_state=42
             )
         ),
         (
